ec8b5adcc28718ae2a668da33e1bd0bc78c6543f
Student <student@achiever69.local>
2016-08-29 12:05:17 +0530

    Initial Commit

---
+++ firstGit1/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  firstGit1
+//
+//  Created by Student on 29/08/16.
+//  Copyright © 2016 Student. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ firstGit1/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  firstGit1
+//
+//  Created by Student on 29/08/16.
+//  Copyright © 2016 Student. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
